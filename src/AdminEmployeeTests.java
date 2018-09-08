b3da626f529c46929466b7b44fef6e33c85ba3a6
Anthony Browne <anthony@adbrowne.co.uk>
2018-09-08 18:51:57 +0100

    Updated the Admin Employee POM and Tests
    Added additional fields and comments to the Admin Employee POM.
    Added further tests to the Admin Employee Test class.

--- src/AdminEmployeeTests.java
+++ src/AdminEmployeeTests.java
@@ -1,14 +1,11 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import org.testng.Assert;
-
 import PageObjects.AdminEmployeePOM;
-
 
 public class AdminEmployeeTests {
     WebDriver driver;
@@ -45,10 +42,131 @@ public class AdminEmployeeTests {
      */
     @Test (priority = 0)
     public void test_searchEmployeeByIDValid() {
-        adminEmployeePOM.setSearchBar("123456");
-        adminEmployeePOM.clickSearchButton();
         String expected = "123456";
+        adminEmployeePOM.setSearchBar(expected);
+        adminEmployeePOM.clickSearchButton();
         String actual = adminEmployeePOM.getEmployeeNumber();
         Assert.assertEquals(expected, actual) ;
+    }
+
+    /**
+     * This test checks that the Search Bar works when entering an Employee ID that doesn't exist.
+     * The search bar is populated with the Employee ID. And then the Search Button is clicked.
+     * Finally we Assert that the Employee Number field on the UI is Empty.
+     */
+    @Test (priority = 0)
+    public void test_searchEmployeeByIDInvalid() {
+        adminEmployeePOM.setSearchBar("125481854811");
+        adminEmployeePOM.clickSearchButton();
+        Assert.assertTrue(adminEmployeePOM.getEmployeeNumber().isEmpty());
+    }
+
+    /**
+     * This test checks that the Search Bar works when entering an Employee Name.
+     * The Search Bar is populated with the Employee Name (Anthony). And the Search Button is then clicked.
+     * Finally, we get the Employee Name from the UI.
+     * An Assertion is made to ensure that the EmployeeName in the field equals Anthony.
+     */
+    @Test (priority = 0)
+    public void test_searchEmployeeByNameValid() {
+        String expected = "Anthony";
+        adminEmployeePOM.setSearchBar(expected);
+        adminEmployeePOM.clickSearchButton();
+        String actual = adminEmployeePOM.getFirstName();
+        Assert.assertEquals(expected, actual);
+    }
+    /**
+     * This test checks that the Search Bar works when entering an Employee Name that doesn't exist.
+     * The search bar is populated with the Employee Name. And then the Search Button is clicked.
+     * Finally we Assert that the Employee Name field on the UI is Empty.
+     */
+    @Test (priority = 0)
+    public void test_searchEmployeeByNameInvalid() {
+        adminEmployeePOM.setSearchBar("Aaabbbcccddd");
+        adminEmployeePOM.clickSearchButton();
+        Assert.assertTrue(adminEmployeePOM.getFirstName().isEmpty());
+    }
+
+    /**
+     * This test creates a new Employee.
+     * It calls the support_CreateEmployee method to create it.
+     * It asserts that each field on the UI matches what was entered in the Test Data.
+     */
+    @Test (priority = 0)
+    public void test_createEmployee(){
+        String firstName = "John";
+        String surName = "Smith";
+        String employeeNumber = "12343356";
+        String email = "john@smith.com";
+        String phoneNumber = "01234567890";
+        support_CreateEmployee(firstName, surName, employeeNumber, email, phoneNumber);
+        Assert.assertEquals(firstName, adminEmployeePOM.getFirstName());
+        Assert.assertEquals(surName, adminEmployeePOM.getSurName());
+        Assert.assertEquals(employeeNumber, adminEmployeePOM.getEmployeeNumber());
+        Assert.assertEquals(email, adminEmployeePOM.getEmail());
+        Assert.assertEquals(phoneNumber, adminEmployeePOM.getPhoneNumber());
+    }
+
+    /**
+     * This test creates then deletes an Employee.
+     * It calls the support_CreateEmployee method.
+     * After Delete has been clicked, it searches for the employee by the ID Number.
+     * Finally we assert that the First Name field is blank.
+     */
+    @Test (priority = 0)
+    public void test_deleteEmployee() {
+        String firstName = "John";
+        String surName = "Smith";
+        String employeeNumber = "12343356";
+        String email = "john@smith.com";
+        String phoneNumber = "01234567890";
+        support_CreateEmployee(firstName, surName, employeeNumber, email, phoneNumber);
+        adminEmployeePOM.clickDelete();
+        adminEmployeePOM.setSearchBar(employeeNumber);
+        adminEmployeePOM.clickSearchButton();
+        Assert.assertTrue(adminEmployeePOM.getFirstName().isEmpty());
+    }
+
+    /**
+     * This test creates then edits an Employee.
+     * It calls the support_CreateEmployee method.
+     * After the employee is created, the edit button is clicked. The value of the Email field is changed.
+     * Save is then clicked.
+     * Finally it Asserts that the Email field is not displaying the new email address.
+     */
+    @Test (priority = 0)
+    public void test_editEmployee() {
+        String firstName = "John";
+        String surName = "Smith";
+        String employeeNumber = "12343356";
+        String email = "john@smith.com";
+        String phoneNumber = "01234567890";
+        String newEmail = "Smith@John.co.uk";
+        support_CreateEmployee(firstName, surName, employeeNumber, email, phoneNumber);
+        adminEmployeePOM.clickEdit();
+        adminEmployeePOM.setEmail(newEmail);
+        adminEmployeePOM.clickSave();
+        Assert.assertEquals(newEmail, adminEmployeePOM.getEmail());
+    }
+
+    /**
+     * This support method creates a New Employee on the UI.
+     * It takes all the data as the parameters then puts it into the Interface.
+     * Finally we click the Create button.
+     * @param firstName String for the Employees First Name
+     * @param surName String for the Employees Surname
+     * @param employeeNumber String for the Employees Phone Number
+     * @param email String for the Employees Email Address
+     * @param phoneNumber String for the Employees phoneNumber
+     */
+    private void support_CreateEmployee(String firstName, String surName, String employeeNumber, String email,
+                                        String phoneNumber) {
+        adminEmployeePOM.clickAdd();
+        adminEmployeePOM.setFirstName(firstName);
+        adminEmployeePOM.setSurName(surName);
+        adminEmployeePOM.setEmployeeNumber(employeeNumber);
+        adminEmployeePOM.setEmail(email);
+        adminEmployeePOM.setPhoneNumber(phoneNumber);
+        adminEmployeePOM.clickCreate();
     }
 }
--- src/PageObjects/AdminEmployeePOM.java
+++ src/PageObjects/AdminEmployeePOM.java
@@ -49,6 +49,14 @@ public class AdminEmployeePOM {
     @FindBy(name="delete")
     WebElement delete;
 
+    // WebElement Object to store the Create Button Object.
+    @FindBy(name="create")
+    WebElement create;
+
+    // WebElement Object to store the Save Button Object.
+    @FindBy(name="save")
+    WebElement save;
+
     /**
      * Initialises the class. Assigns the provided driver to the local class.
      * Initialises the PageFactory for the class variables.
@@ -113,35 +121,79 @@ public class AdminEmployeePOM {
         return surName.getText();
     }
 
+    /**
+     * Sets the value of the Employee Number field.
+     * @param employeeNumberString String with the Employee Number.
+     */
     public void setEmployeeNumber(String employeeNumberString) {
         employeeNumber.sendKeys(employeeNumberString);
     }
 
+    /**
+     * Gets the value of the Employee Number field.
+     * @return String with the Employee Number.
+     */
     public String getEmployeeNumber() {
         return employeeNumber.getText();
     }
 
+    /**
+     * Sets the value of the Phone Number field.
+     * @param phoneNumberString The value of the Phone Number to be entered.
+     */
     public void setPhoneNumber(String phoneNumberString) {
         phoneNumber.sendKeys(phoneNumberString);
     }
 
+    /**
+     * Gets the value of the Phone Number field.
+     * @return String with the Phone Number.
+     */
     public String getPhoneNumber() {
         return phoneNumber.getText();
     }
 
+    /**
+     * Sets the value of the Email field.
+     * @param emailString The value of the Email field to be entered.
+     */
     public void setEmail(String emailString) {
         email.sendKeys(emailString);
     }
 
+    /**
+     * Gets the value of the Email field.
+     * @return String with the value of the Email field.
+     */
     public String getEmail() {
         return email.getText();
     }
 
+    /**
+     * Clicks on the Edit button on the UI.
+     */
     public void clickEdit() {
         edit.click();
     }
 
+    /**
+     * Clicks on the Delete button on the UI.
+     */
     public void clickDelete() {
         delete.click();
+    }
+
+    /**
+     * Clicks on the Create button on the UI.
+     */
+    public void clickCreate() {
+        create.click();
+    }
+
+    /**
+     * Clicks on the Save button on the UI.
+     */
+    public void clickSave() {
+        save.click();
     }
 }
